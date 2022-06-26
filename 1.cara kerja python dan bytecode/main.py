f7ac601e9acccfc3b4fb6db3e041abafc61a7197
lutfiazhar000 <asharilutfi71@gmail.com>
2022-06-26 11:23:48 +0700

    first commit

---
+++ 1.cara kerja python dan bytecode/main.py
@@ -0,0 +1,18 @@
+import time
+start_time = time.time()
+
+print ("hello")
+print ("world")
+print ("hello world")
+
+print ("hello Chantiek")
+# ini adalah comment
+a = 10 # ini adalah comment juga
+""" ada apa dengan ucup dan otong si ganteng dalam multiline """
+
+print(a)
+print(time.time() - start_time, "detik")
+#  kita bisa mengcompile python ke 
+#  yang namanya bytecode
+#  cara mengcompile, buka terminal dan tuliskan 
+# python -m py_compile main.py
